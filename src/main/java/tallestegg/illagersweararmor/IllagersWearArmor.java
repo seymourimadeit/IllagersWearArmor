f993c9f864527fca243157d720850d630d681fbf
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-07-23 20:31:46 -0700

    wertyui

---
+++ src/main/java/tallestegg/illagersweararmor/IllagersWearArmor.java
@@ -0,0 +1,57 @@
+package tallestegg.illagersweararmor;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(IllagersWearArmor.MODID)
+public class IllagersWearArmor
+{
+	public static final String MODID = "illagersweararmor";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public IllagersWearArmor() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+
+    }
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) 
+    {
+    	
+    }
+}
