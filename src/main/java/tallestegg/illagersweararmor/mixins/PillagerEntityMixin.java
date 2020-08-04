5f3f0c1107ddf95e1511fff8536523b29d56de73
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2020-08-03 21:30:22 -0700

    this one line in the json cost me 10 minutes of my life, bruh.

--- src/main/java/tallestegg/illagersweararmor/mixins/PillagerEntityMixin.java
+++ src/main/java/tallestegg/illagersweararmor/mixins/PillagerEntityMixin.java
@@ -1,5 +1,10 @@
 package tallestegg.illagersweararmor.mixins;
 
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.entity.monster.PillagerEntity;
+
+@Mixin(PillagerEntity.class)
 public class PillagerEntityMixin {
 
 }
