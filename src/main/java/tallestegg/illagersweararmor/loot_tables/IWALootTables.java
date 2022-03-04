7e85c8cd6b7d612b9c26b937b6290c8f2a906c0b
seymourimadeit <55965249+seymourimadeit@users.noreply.github.com>
2022-03-03 19:30:12 -0800

    fixed #7 and #6

--- src/main/java/tallestegg/illagersweararmor/loot_tables/IWALootTables.java
+++ src/main/java/tallestegg/illagersweararmor/loot_tables/IWALootTables.java
@@ -1,8 +1,12 @@
 package tallestegg.illagersweararmor.loot_tables;
 
+import java.util.function.Consumer;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
@@ -10,7 +14,8 @@ import tallestegg.illagersweararmor.IllagersWearArmor;
 
 @EventBusSubscriber(modid = IllagersWearArmor.MODID, bus = Bus.MOD)
 public class IWALootTables {
-    public static final LootContextParamSet SLOT = LootContextParamSets.register("slot", (table) -> {
+    public static final BiMap<ResourceLocation, LootContextParamSet> REGISTRY = HashBiMap.create();
+    public static final LootContextParamSet SLOT = register("slot", (table) -> {
         table.required(LootContextParams.THIS_ENTITY);
     });
 
@@ -22,4 +27,17 @@ public class IWALootTables {
             "entities/illager_legs");
     public static final ResourceLocation ILLAGER_FEET = new ResourceLocation(IllagersWearArmor.MODID,
             "entities/illager_feet");
+    
+    public static LootContextParamSet register(String p_81429_, Consumer<LootContextParamSet.Builder> p_81430_) {
+        LootContextParamSet.Builder lootcontextparamset$builder = new LootContextParamSet.Builder();
+        p_81430_.accept(lootcontextparamset$builder);
+        LootContextParamSet lootcontextparamset = lootcontextparamset$builder.build();
+        ResourceLocation resourcelocation = new ResourceLocation(IllagersWearArmor.MODID + p_81429_);
+        LootContextParamSet lootcontextparamset1 = REGISTRY.put(resourcelocation, lootcontextparamset);
+        if (lootcontextparamset1 != null) {
+           throw new IllegalStateException("Loot table parameter set " + resourcelocation + " is already registered");
+        } else {
+           return lootcontextparamset;
+        }
+     }
 }
