0ef49a14e2e58788997e3c82bab1a8ae6f8bd975
seymourimadeit <anemailaccount@mail.com>
2024-05-20 07:04:17 -0700

    fix #35

--- src/main/java/tallestegg/illagersweararmor/IWAHelper.java
+++ src/main/java/tallestegg/illagersweararmor/IWAHelper.java
@@ -3,22 +3,23 @@ package tallestegg.illagersweararmor;
 public class IWAHelper {
     public static float getWaveArmorChances(int waves) {
         switch (waves) {
-        case 0:
-            return IWAConfig.Wave1Chances;
         case 1:
-            return IWAConfig.Wave2Chances;
+            return IWAConfig.Wave1Chances;
         case 2:
-            return IWAConfig.Wave3Chances;
+            return IWAConfig.Wave2Chances;
         case 3:
-            return IWAConfig.Wave4Chances;
+            return IWAConfig.Wave3Chances;
         case 4:
-            return IWAConfig.Wave5Chances;
+            return IWAConfig.Wave4Chances;
         case 5:
-            return IWAConfig.Wave6Chances;
+            return IWAConfig.Wave5Chances;
         case 6:
-            return IWAConfig.Wave7Chances;
+            return IWAConfig.Wave6Chances;
         case 7:
+            return IWAConfig.Wave7Chances;
+        case 8:
             return IWAConfig.Wave8Chances;
+
         }
         return 0;
     }
